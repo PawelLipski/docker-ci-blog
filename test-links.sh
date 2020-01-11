2f989526a28330fde4200d33e54d295803b584a2
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-01-11 18:14:47 +0100

    Fix test-links.sh

--- test-links.sh
+++ test-links.sh
@@ -2,4 +2,4 @@
 
 set -e -o pipefail -u
 
-grep -Po '(?<=\]\().*//.*(?=\))' blog.md | xargs -t -l curl -fsI -o/dev/null -w "> %{http_code}\n"
+grep -Po '(?<=\]\().*//.*?(?=\))' blog.md | xargs -t -l curl -fsI -o/dev/null -w "> %{http_code}\n"
